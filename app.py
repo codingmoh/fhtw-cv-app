f3775e242e5ada267e8ff810420f1979bba6851d
coding.moh@gmail.com <coding.moh@gmail.com>
2021-12-13 22:45:14 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import io
+import os
+import cv2
+import uvicorn
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import numpy as np
+import nest_asyncio
+from enum import Enum
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import StreamingResponse
+
+app = FastAPI(title='Deploying a ML Model with FastAPI')
+
+# List available models using Enum for convenience. This is useful when the options are pre-defined.
+class Model(str, Enum):
+    yolov3tiny = "yolov3-tiny"
+    yolov3 = "yolov3"
+
+
+# By using @app.get("/") you are allowing the GET method to work for the / endpoint.
+@app.get("/")
+def home():
+    return "Head over to http://localhost:8000/docs."
+
+
+# This endpoint handles all the logic necessary for the object detection to work.
+# It requires the desired model and the image in which to perform object detection.
+@app.post("/predict") 
+def prediction(model: Model, file: UploadFile = File(...)):
+
+    # 1. VALIDATE INPUT FILE
+    filename = file.filename
+    fileExtension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not fileExtension:
+        raise HTTPException(status_code=415, detail="Unsupported file provided.")
+    
+    # 2. TRANSFORM RAW IMAGE INTO CV2 image
+    
+    # Read image as a stream of bytes
+    image_stream = io.BytesIO(file.file.read())
+    
+    # Start the stream from the beginning (position zero)
+    image_stream.seek(0)
+    
+    # Write the stream of bytes into a numpy array
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    
+    # Decode the numpy array as an image
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    
+    # 3. RUN OBJECT DETECTION MODEL
+    
+    # Run object detection
+    bbox, label, conf = cv.detect_common_objects(image, model=model)
+    
+    # Create image that includes bounding boxes and labels
+    output_image = draw_bbox(image, bbox, label, conf)
+    
+    # 4. STREAM THE RESPONSE BACK TO THE CLIENT
+    _, img = cv2.imencode(".jpg", output_image)
+    #image_bytes = io.BytesIO(img)
+    # Return the image as a stream specifying media type
+    return StreamingResponse(io.BytesIO(img), media_type="image/jpeg")
+
+# Allows the server to be run in this interactive environment
+nest_asyncio.apply()
+
+host = "0.0.0.0" 
+port = os.environ["APP_PORT"]
+# Spin up the server!    
+uvicorn.run(app, host=host, port=port)
